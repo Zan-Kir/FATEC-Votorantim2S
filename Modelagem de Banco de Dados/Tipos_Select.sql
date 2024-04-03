9600e11921e766a0fefd6f6278ccd2525e4634d1
Zan-Kir <murilo4santos@hotmail.com>
2024-04-02 22:14:16 -0300

    BDR-02-04

--- Modelagem de Banco de Dados/Tipos_Select.sql
+++ Modelagem de Banco de Dados/Tipos_Select.sql
@@ -47,3 +47,47 @@ Select c.nome_cliente, p.num_pedido
 from cliente c
 Full outer join pedido p on c.Cod_cliente = p.cod_cliente
 Where c.Cod_cliente is null or p.cod_cliente is null
+
+Select f.primeiro_nome, f.salario, s.nome_setor
+from Funcionario f
+Cross join Setor s
+Order by f.primeiro_nome
+
+Select c.nome_cliente, p.num_pedido
+from cliente c
+Cross join pedido p
+Order by c.Nome_cliente
+
+Select min(salario_fixo) as 'Menor Salario', max(salario_fixo) as 'Maior Salario'
+from vendedor
+
+Select Sum(quantidade)
+from item_pedido
+Where cod_produto = 3
+
+Select avg(salario_fixo) as MEDIA_SALARIO
+from vendedor
+
+Select count(*) from vendedor
+Where salario_fixo > 2500
+
+Select num_pedido, total_produtos = count(*)
+from item_pedido
+Group by num_pedido
+
+Select num_pedido, total_produtos = count(*)
+from item_pedido
+where quantidade > 5
+Group by num_pedido
+having count(*) > 1
+
+Update produto
+set valor_unitario = 4.00
+where descricao = 'alface'
+
+update produto
+set Valor_unitario = Valor_unitario * 1.025
+where Valor_unitario <
+	(select avg(Valor_unitario)
+	from produto
+	where unidade = 'KG')
