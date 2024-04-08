91e0a77437517111fd77f828b36a281ae7de9e69
Zan-Kir <murilo4santos@hotmail.com>
2024-04-08 18:07:00 -0300

    ListaDuplamenteLigada

--- Técnicas de Programação/Aula2/src/Aula5/ListaDuplamenteLigada.java
+++ Técnicas de Programação/Aula2/src/Aula5/ListaDuplamenteLigada.java
@@ -18,7 +18,7 @@ public class ListaDuplamenteLigada {
         primeiroNo= ultimoNo = null;
     }
     
-    public void add(Object elemento){
+    public void adicionar(Object elemento){
         //Adiciona um nó na ultima posição.
         //Para o 1º Nó
         NoDuplo aux = new NoDuplo(elemento, null, null);
@@ -33,81 +33,101 @@ public class ListaDuplamenteLigada {
             tamanhoLista++;
         }
     }
-    public void add(Object elemento, int indice){
+    public void adicionar(Object elemento, int indice){
         /*Método deverá adicionar o elemento na posição
         indicada pelo indice */
         
         //checa se a posição é válida
         if (indice > tamanhoLista || indice < 0){
             System.out.println("Erro indice inválido");
+            return;
         }
-        
+
         //cria um novo Nó
         NoDuplo novo = new NoDuplo(elemento, null, null);
         
         //define se é o Primeiro Nó
         if (indice == 0) {
-           novo.setProximoNo(primeiroNo);
-           novo.setAnteriorNo(null);
-           primeiroNo = novo;
+            novo.setProximoNo(primeiroNo);
+            novo.setAnteriorNo(null);
+            primeiroNo = novo;
         }
         
         //define em outra posição
         else {
-            NoDuplo atual = primeiroNo;
+            NoDuplo anterior = percorreLista(indice -1);
             //indice -1 pra achar o nó anterior que é onde sera inserido
             
-            for(int i = 0; i < indice; i++) {
-                  atual = atual.getProximoNo();
+            //checa se o  nó é válido
+            if (anterior == null) {
+                System.out.println("Erro: índice inválido");
+                return;
             }
             
-            novo.setProximoNo(atual.getProximoNo()); //esse faz o atual apontar pro proximo
-            novo.setAnteriorNo(atual.getAnteriorNo());
-            atual.setProximoNo(novo); // esse faz o anterior apontar pro atual
+            novo.setProximoNo(anterior.getProximoNo()); //esse faz o novo apontar pro proximo
+            novo.setAnteriorNo(anterior); // faz o novo apontar para o nó anterior
+            
+            //checa se não é a ultima posição  para fazer o proximo apontar para o novo
+            if (anterior.getProximoNo() != null) {
+                anterior.getProximoNo().setAnteriorNo(novo); //faz o ex nó atual apontar pro novo
+            }
+            anterior.setProximoNo(novo); //faz o nó anterior apontar pro novo
         }
         
         //define ultimo nó se for colocado no final da lista
         if (indice == tamanhoLista) {
             ultimoNo = novo;
         }
+        
         //acrescenta no total da lista
          tamanhoLista++;
     }
+    
     public Object remover(int indice){
         /*Remove o elemento indicado pelo indice 
         e retorna esse objeto*/
         
         //checa se a posição é válida
-        if (indice > tamanhoLista || indice < 0){
+        if (indice >= tamanhoLista || indice < 0){
             System.out.println("Erro indice inválido");
             return null;
         }
         
-        //pega posição a ser removida e o nó anterior
-        NoDuplo atual = primeiroNo;
-        NoDuplo anterior = null;
-        for(int i = 0; i < indice; i++) {
-            anterior = atual;
-            atual = atual.getProximoNo();
-        }
+        NoDuplo atual;
         
+        
+        //pega posição a ser removida e o nó anterior
+        //atual = percorreLista(indice);
+       
         //se for a primeira posição
          if (indice == 0) {
-            primeiroNo  = atual.getProximoNo();
+             atual = primeiroNo;
+             primeiroNo  = atual.getProximoNo(); //o segundo nó é atribuido ao primeiro nó
+             primeiroNo.setAnteriorNo(null); //define o anterior como null, pois é a primeira posição
         }
          //define o Nó anterior a posição atual para o próximo
          else {
-            anterior.setProximoNo(atual.getProximoNo());
-         }   
-         
-         //corrigir se o removido for o ultimo
-         if (indice == tamanhoLista - 1) {
-             ultimoNo = anterior;
+             NoDuplo anterior = percorreLista(indice -1);
+             atual = anterior.getProximoNo();
+             anterior.setProximoNo(atual.getProximoNo());
+             
+             if(atual.getProximoNo() != null){
+                 (atual.getProximoNo()).setAnteriorNo(anterior);
+                 // define o nó anterior do próximo nó do atual como o anterior a ele
+                //(proximo do atual . definir  nó anterior (anterior)
+                //resumindo faz o proximo nó apontar para o anterior (exclui o atual)
+             }
+             else {  //se for o último
+                 ultimoNo = anterior;
+                 ultimoNo.setProximoNo(null);
+             }
          }
+         
          tamanhoLista--;
         return atual.getInfo();
     }
-    public Object get(int indice){
+    
+    public Object getLista(int indice){
         //Recebe o índice iniciando em zero
         // e retorna o campo Info do Nó
         // correspondente.
@@ -120,11 +140,11 @@ public class ListaDuplamenteLigada {
         else return null;
         
     }
-    public String concatenaNos(){
+    public String imprimirLista(){
         /*Retorna o Info de todos os Nós concatenados*/
         String concatena = "Lista de nos: \n";
         for (int i = 0; i < tamanhoLista; i++ ) {
-            concatena += get(i) + "\n";
+            concatena += getLista(i) + "\n";
         }
         
         return concatena;
@@ -133,10 +153,8 @@ public class ListaDuplamenteLigada {
     private NoDuplo percorreLista(int indice) {
         
         NoDuplo aux=primeiroNo;
-        NoDuplo anterior = null;
         for(int i=0; i<indice;i++){
             aux=aux.getProximoNo();
-            anterior=anterior.getAnteriorNo();
         }
         return aux;
     }
@@ -144,4 +162,34 @@ public class ListaDuplamenteLigada {
     public int getTamanhoLista() {
         return tamanhoLista;
     }  
+
+    public void adicionaOrdenado(int valor, TipoOrdenacao tipo) {
+        
+        /*if (tamanhoLista == 0) {
+            primeiroNo = ultimoNo = null;
+        }
+        */
+        
+        if (tipo == TipoOrdenacao.CRESCENTE) {
+            
+            if (tamanhoLista == 0 || valor <= (int) primeiroNo.getInfo()) {
+                adicionar(valor, 0); // define se for o primeiro elemento da lista
+                return;
+            }
+            
+            NoDuplo atual = primeiroNo; //inicia o atual no começo
+            int indice = 0; //contador de posicao
+            while (atual.getProximoNo() != null && (int) atual.getProximoNo().getInfo() < valor) {
+                // enquanto o atual nao for o ultimo E o valor(info) do atual for < que o valor
+                atual = atual.getProximoNo();
+                indice++;
+            }
+            adicionar(valor, indice +1);            
+        }
+        
+        if (tipo == TipoOrdenacao.DESCRESCENTE) {
+            
+        }
+        System.out.println(this.imprimirLista());
+    }
 }
--- Técnicas de Programação/Aula2/src/Aula5/Principal.java
+++ Técnicas de Programação/Aula2/src/Aula5/Principal.java
@@ -21,10 +21,23 @@ public class Principal {
         System.out.println(minhaLista.concatenaNos());
                 
         ListaDuplamenteLigada listaDupla = new ListaDuplamenteLigada();
-        listaDupla.add("Banana");
-        listaDupla.add("Maca", 0);
-        listaDupla.add("Pera", 1);
-        System.out.println(listaDupla);
+        listaDupla.adicionar("Morango");
+        listaDupla.adicionar("Uva", 0);
+        listaDupla.adicionar("Mirtilo", 1);
+        listaDupla.adicionar("Caju", 1);
+        System.out.println(listaDupla.imprimirLista());
+        listaDupla.remover(2);
+        System.out.println(listaDupla.imprimirLista());
+        
+        ListaDuplamenteLigada listaOrdenada  = new ListaDuplamenteLigada();
+        listaOrdenada.adicionaOrdenado(0, TipoOrdenacao.CRESCENTE);
+        listaOrdenada.adicionaOrdenado(1, TipoOrdenacao.CRESCENTE);
+        listaOrdenada.adicionaOrdenado(3, TipoOrdenacao.CRESCENTE);
+        listaOrdenada.adicionaOrdenado(2, TipoOrdenacao.CRESCENTE);
+        listaOrdenada.adicionaOrdenado(1, TipoOrdenacao.CRESCENTE);
+        listaOrdenada.adicionaOrdenado(3, TipoOrdenacao.CRESCENTE);
+    /*     listaOrdenada.adicionaOrdenado(1, TipoOrdenacao.CRESCENTE);
+*/
     }
     
 }
