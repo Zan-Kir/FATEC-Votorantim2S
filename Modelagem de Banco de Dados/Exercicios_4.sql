7a1987984c106a51866bc8d79aaf88f86a5da3aa
Zan-Kir <murilo4santos@hotmail.com>
2024-04-17 22:30:47 -0300

    EngII-17-04

--- Modelagem de Banco de Dados/Exercicios_4.sql
+++ Modelagem de Banco de Dados/Exercicios_4.sql
@@ -32,7 +32,32 @@ where f.FILME = 'A vida é bela'
 group by f.FILME, f.VALOR_LOCACAO
 
 /* 7 */
+Select f.DIRETOR,  count(l.DATA_RETIRADA) as Saidas from FILME f
+inner Join LOCACOES l on f.COD_FILME = l.COD_FILME
+group by f.DIRETOR
 
+/* 8 */
+Select f.FILME, cat.NOME_CATEGORIA from CATEGORIA cat
+inner join FILME f on f.COD_CATEGORIA = cat.COD_CATEGORIA
+inner join LOCACOES l on l.COD_FILME = f.COD_FILME
+inner join CLIENTES c on l.COD_CLIENTE = c.COD_CLIENTE
+where c.NOME = 'Maria Chiquinha'
+
+
+/* 9 */
+Select c.NOME, c.TELEFONE from CLIENTES c
+inner join LOCACOES l on l.COD_CLIENTE = c.COD_CLIENTE
+inner Join FILME f on f.COD_FILME = l.COD_FILME
+where f.FILME = 'Constantine'
+
+/* 10 */
+Select cat.NOME_CATEGORIA from CATEGORIA cat
+left join FILME f on f.COD_CATEGORIA = cat.COD_CATEGORIA
+where f.COD_CATEGORIA = null
 
 select * from FILME
 select * from LOCACOES
+select * from CLIENTES
+select * from CATEGORIA
+
+
