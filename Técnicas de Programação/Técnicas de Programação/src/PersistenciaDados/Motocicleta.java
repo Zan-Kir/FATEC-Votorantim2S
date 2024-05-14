9595d8c1d00371424bd073acc85dbd3bdd83fb7e
Zan-Kir <murilo4santos@hotmail.com>
2024-05-13 22:02:47 -0300

    TP-13-05

---
+++ Técnicas de Programação/Técnicas de Programação/src/PersistenciaDados/Motocicleta.java
@@ -0,0 +1,34 @@
+
+package PersistenciaDados;
+
+import java.io.Serializable;
+
+
+public class Motocicleta implements Serializable {
+    //atributos
+    private String marca, modelo;
+    private float velocidade;
+    
+    //construtor
+    public Motocicleta(String marca, String modelo, float velocidade) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidade = velocidade;
+    }
+    
+    //metodos
+    public float alterarVelocidade(float fatorAceleracao) {
+        velocidade = velocidade*fatorAceleracao;
+        return velocidade;
+    }
+    
+    public float parar() {
+        velocidade = 0;
+        return velocidade;
+    }
+
+    public float getVelocidade() {
+        return this.velocidade;
+    }
+    
+}
